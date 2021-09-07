249b4dedcb9d197c26eb2c979ca77905632f6aa1
Nilov Lev <nilovlev@yandex.ru>
2021-09-08 02:34:08 +0300

    Add MainController

---
+++ Base/MainController.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import *
+from UIManager import UIManager
+from common.singleton import singleton
+
+@singleton
+class MainController:
+    ui_manager: UIManager
+    main_window: QMainWindow
+
+    def __init__(self):
+        self.ui_manager = UIManager()
+        self.main_window = self.ui_manager.main_window
+
+
+MainController()
