6e2c605dff10f0793570465fcef1cd1fe6cf1c62
yveega <47260215+yveega@users.noreply.github.com>
2021-09-08 04:42:37 +0300

    test kolya push

--- Renderer/__init__.py
+++ Renderer/__init__.py
@@ -5,15 +5,18 @@ import sys
 from Renderer.SubRenderers.text_render import ColoredText
 from Renderer.SubRenderers.graph_render import GraphPainter
 
+render_tags = {
+    "text": ColoredText,
+    "graph": GraphPainter
+}
+
 
 def render(layout: QFormLayout, data: ET.Element):
-    # root = data.getroot()
+    for i in range(len(layout)):
+        layout.removeRow(i)
     for child in data:
         print(child.tag)
-        if child.tag == "text":
-            layout.addRow(ColoredText(child))
-        elif child.tag == "graph":
-            layout.addRow(GraphPainter(child))
+        layout.addRow(render_tags[child.tag](child))
 
 
 if __name__ == '__main__':
@@ -37,7 +40,36 @@ if __name__ == '__main__':
     root = ET.Element("root")
     text1 = ET.SubElement(root, "text")
     ET.SubElement(text1, "sub_text", color="red").text = "abc"
-    ET.SubElement(text1, "sub_text", color="blue").text = "def"
+    ET.SubElement(text1, "sub_text", color="blue").text = "d34563658876ef"
+    ET.SubElement(text1, "sub_text", color="lightblue").text = "123"
+    ET.SubElement(text1, "sub_text", color="white").text = "12 9764 dgee3"
+    ET.SubElement(text1, "sub_text", color="red").text = "abc"
+    ET.SubElement(text1, "sub_text", color="blue").text = "d345636 58876ef"
+    ET.SubElement(text1, "sub_text", color="lightblue").text = "123"
+    ET.SubElement(text1, "enter")
+    ET.SubElement(text1, "enter")
+    ET.SubElement(text1, "sub_text", color="white").text = "123 23256"
+    ET.SubElement(text1, "sub_text", color="red").text = "abcsdg htu hdzr"
+    ET.SubElement(text1, "sub_text", color="blue").text = "d345636 58876ef"
+    ET.SubElement(text1, "sub_text", color="lightblue").text = "123"
+    ET.SubElement(text1, "sub_text", color="white").text = "123"
+    text2 = ET.SubElement(root, "text")
+    ET.SubElement(text2, "sub_text", color="white").text = "hello"
+    ET.SubElement(text2, "sub_text", color="pink").text = "world"
+    ET.SubElement(text2, "sub_text", color="green").text = "псы"
+    ET.SubElement(root, "graph").text = "12 55 17 66 25 77 28 88"
+
+    root1 = ET.Element("root")
+    text1 = ET.SubElement(root, "text")
+    ET.SubElement(text1, "sub_text", color="red").text = "abc"
+    ET.SubElement(text1, "sub_text", color="blue").text = "d348876ef"
+    ET.SubElement(text1, "sub_text", color="white").text = "12 ee3"
+    ET.SubElement(text1, "sub_text", color="red").text = "abc"
+    ET.SubElement(text1, "sub_text", color="lightblue").text = "123"
+    ET.SubElement(text1, "enter")
+    ET.SubElement(text1, "enter")
+    ET.SubElement(text1, "sub_text", color="white").text = "123 23256"
+    ET.SubElement(text1, "sub_text", color="blue").text = "d6 58876ef"
     ET.SubElement(text1, "sub_text", color="lightblue").text = "123"
     ET.SubElement(text1, "sub_text", color="white").text = "123"
     text2 = ET.SubElement(root, "text")
@@ -49,5 +81,6 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = MainWindow()
     render(win.form, root)
+    render(win.form, root1)
     win.show()
     sys.exit(app.exec_())
