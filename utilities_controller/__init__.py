3ffe07d2a31c8cc652fb4708ed5476ab6683b5b5
Alexey <alexey.nebolsin.jr@gmail.com>
2021-09-08 03:48:24 +0300

    with previous commit

--- utilities_controller/__init__.py
+++ utilities_controller/__init__.py
@@ -1,5 +1,6 @@
 import os, sys
 import threading
+import shutil, ntpath
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../ExternalUtilities')))
 
@@ -22,3 +23,17 @@ def import_utility(utName: str, newThread = False, **kwargs):
         p = threading.Thread(target=new_thread_utility, args=(instance,))
         p.start()
         return instance
+
+def install_utility(path: str) -> str:
+    try:
+        inpFile = 'test_new_util_x01'
+        shutil.copy(path, './ExternalUtilities/' + inpFile + '.py')
+        import_utility(inpFile)
+        shutil.copy( './ExternalUtilities/' + inpFile + '.py', './ExternalUtilities/' + ntpath.basename(path))
+        os.remove(path)
+        os.remove('./ExternalUtilities/' + inpFile + '.py')
+        return 'Installation succeeded'
+    except:
+        return 'Bad installation'
+
+print(install_utility("C:/Users/alexe/Downloads/snake_game.py"))
